d0110c312961871535465ae8a67ffcdc93ce2c0f
Claire Kodia <claire.kodia@gmail.com>
2020-03-31 17:09:01 +0200

    final wokring version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Contact name = "Hilda Sutton" avatar = "https://randomuser.me/api/portraits/women/47.jpg" online/>
-      <Contact name = "Eli Ramos" avatar = "https://randomuser.me/api/portraits/men/51.jpg" online/>
+      <Contact name = "Eli Ramos" avatar = "https://randomuser.me/api/portraits/men/51.jpg" />
       <Contact name = "Justin Porter" avatar = "https://randomuser.me/api/portraits/men/48.jpg" online/>
     </div>
   );
