757d715afa042588c03c25534ec4e185db3e553f
Claire Kodia <claire.kodia@gmail.com>
2020-03-31 17:51:08 +0200

    Call the ContactList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 
 function App() {
   return (
     <div className="App">
-      <Contact name = "Hilda Sutton" avatar = "https://randomuser.me/api/portraits/women/47.jpg" online/>
-      <Contact name = "Eli Ramos" avatar = "https://randomuser.me/api/portraits/men/51.jpg" />
-      <Contact name = "Justin Porter" avatar = "https://randomuser.me/api/portraits/men/48.jpg" online/>
+      <ContactList />
+      
     </div>
   );
 }
