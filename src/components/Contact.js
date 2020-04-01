80376c0a46fc775afb923c396182899469439a8d
Claire Kodia <claire.kodia@gmail.com>
2020-04-01 18:32:27 +0200

    Added a State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,18 +4,35 @@ import urlPropType from 'url-prop-type';
 import './Contact.css';
 
 
-const Contact = (props) => (
-    <div className="Contact">
-        <img className="avatar" src={props.avatar} alt={props.name} />
-        <div>
-            <h4 className="name">{props.name}</h4>
-            <div className="status">
-                <div className={props.online? "status-online" : "status-offline"}></div>
-                <p className="status-text">{props.online? "online" : "offline"}</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          online: false,
+        };
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status" onClick={event => { 
+                            const newOnline = !this.state.online;
+                            this.setState({ online: newOnline});}}>
+                        <div className={this.state.online? "status-online" : "status-offline"}></div>
+                        <p className="status-text">{this.state.online? "online" : "offline"}</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    );
+            );
+    }
+    
+};
+
+
+
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
